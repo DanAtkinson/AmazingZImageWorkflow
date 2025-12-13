6ad0ad42886ffe6ffc717e98f1584b4a59aaf1d0
Martin Rizzo <martinrizzo@gmail.com>
2025-12-12 23:26:32 -0300

    chore: Refine automated release ZIP naming
    This commit improves the automation process for generating release ZIP files
    by ensuring their names match the corresponding version tag. For example, a
    tag 'v1.2.3' will produce a file named "amazingZImage_v12.zip".

--- files/utils/release.sh
+++ files/utils/release.sh
@@ -12,14 +12,28 @@
 
 
 zip_z_comics() {
-    local VERSION=${1:-v1.0.0}
-    local ZCOMICS_ZIP="$2/amazing-z-comics.zip"
-    local ZIMAGE_ZIP="$2/amazing-z-image.zip"
+    local VERSION=${1:-v1.2.3}
+    local MAJOR MINOR
+    MAJOR=$(echo "${VERSION##v}" | cut -d '.' -f1)
+    MINOR=$(echo "${VERSION##v}" | cut -d '.' -f2)
+
+    local ZCOMICS_ZIP="$2/amazingZComics_v${MAJOR}${MINOR}.zip"
+    local ZIMAGE_ZIP="$2/amazingZImage_v${MAJOR}${MINOR}.zip"
 
     # busca amazing-z-comics_GGUF.json y amazin-z-comics_SAFETENSORS.json en el directorio actual
     # y los empaquete en un zip en el directorio OUTPUT_DIR
-    zip -j "$ZCOMICS_ZIP" "amazing-z-comics_GGUF.json" "amazing-z-comics_SAFETENSORS.json"
-    zip -j "$ZIMAGE_ZIP" "amazing-z-image_GGUF.json" "amazing-z-image_SAFETENSORS.json"
+    zip -j "$ZCOMICS_ZIP" \
+        "amazing-z-comics_GGUF.json" \
+        "amazing-z-comics_SAFETENSORS.json" \
+        "LICENSE" \
+        "amazing-z-comics_GGUF.png"
+
+
+    zip -j "$ZIMAGE_ZIP" \
+        "amazing-z-image_GGUF.json" \
+        "amazing-z-image_SAFETENSORS.json" \
+        "LICENSE" \
+        "amazing-z-image_GGUF.png"
 }
 
 #===========================================================================#
@@ -27,7 +41,7 @@ zip_z_comics() {
 #===========================================================================#
 
 # este valor es retornado (no es local)
-export RELEASE_DIR="${2:-/tmp/}amazing_release"
+export RELEASE_DIR="${2:-/tmp}/amazing_release"
 mkdir -p "$RELEASE_DIR"
 
 zip_z_comics "$1" "$RELEASE_DIR"
